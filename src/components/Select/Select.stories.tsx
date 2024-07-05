6b8713b96d2f0541382869137bea152964cf5c92
Dmitriy <goglev37@gmail.com>
2024-07-05 22:03:06 +0300

    select by Dimych

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,28 @@
+import {action} from "@storybook/addon-actions";
+import {SelectByDimych} from "./SelectByDimych";
+import {useState} from "react";
+
+export default {
+    title: "Select",
+    component: SelectByDimych
+}
+export const Base = () => {
+    const [value, setValue] = useState("2")
+    return <>
+
+        <SelectByDimych items={[
+            {value: "1", title: "Minsk"},
+            {value: "2", title: "Msk"}
+        ]} value={value} onChange={setValue}/>
+    </>
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null)
+    return <>
+        <SelectByDimych value={value} items={[
+            {value: "1", title: "Minsk"},
+            {value: "1", title: "Msk"}
+        ]} onChange={setValue}/>
+    </>
+}
