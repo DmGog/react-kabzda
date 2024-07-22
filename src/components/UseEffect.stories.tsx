afcc911957555767733cbd1d33960b2240447db9
Dmitriy <goglev37@gmail.com>
2024-07-22 13:01:30 +0300

    useEffect

--- src/components/UseEffect.stories.tsx
+++ src/components/UseEffect.stories.tsx
@@ -9,9 +9,24 @@ export const Example = () => {
     console.log("example1")
 
     const [counter, setCounter] = useState(0)
+
+    // ******* сработает каждый раз после отрисовки компоненты ***********
+
+    /* useEffect(() => {
+         console.log("UseEffect")
+     }, );*/
+
+    // ******** сработает один раз после отрисовки компоненты *****************
+
+    /*useEffect(() => {
+        console.log("UseEffect")
+    }, []);*/
+
+    // ******** сработает при изменении зависимостей **********
+
     useEffect(() => {
         console.log("UseEffect")
-    }, []);
+    }, [counter]);
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
